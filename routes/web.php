a8f7f65ad72d0d81e0f913a7783a3a37f86e91ea
ChanVimean <mrrmeanvlogger@gmail.com>
2026-05-26 19:38:15 +0700

    test 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'LATEST CODE 123',
+        'time' => now(),
+    ]);
+});
+
+Route::get('/version', function () {
+    return response()->json([
+        'version' => 'web-v1',
+        'time' => now(),
+    ]);
 });

59ec5025d1f1fef16223c2a354b6165b2532bc30
ChanVimean <mrrmeanvlogger@gmail.com>
2026-05-26 12:58:09 +0700

    migrate 50 data

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
+Route::get('/movie', [MovieController::class, 'index']);
